bbfbe12f9903aa9df79445a2c6f9e6cc079626c0
Aleksandr Bernikov <bernikov_1993@mail.ru>
2021-07-15 15:02:42 +0500

    1.

--- app/src/main/java/com/example/mp3message/UserModel.java
+++ app/src/main/java/com/example/mp3message/UserModel.java
@@ -1,43 +1,19 @@
 package com.example.mp3message;
 
-import android.os.Environment;
-
 import java.io.File;
-import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-//Считываю данные из директории Download, отбираю только MP3 файлы
+//Считываю данные из директории
 public class UserModel {
-
-    //Строка название директории
-    private final String path = Environment.getExternalStorageDirectory().toString() + "/Download/";
-    File directory = new File(path);
-
-    //Читаем директорию и заполняем List<Name>
-    public List<String> readDir() {
-        List<String> listMusicName = new ArrayList<>();
+    //Читаем директорию и заполняем List<String>
+    public List<File> readDir(String path) {
+        File directory = new File(path);
+        List<File> listMusicName = new ArrayList<>();
         File[] files = directory.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (accept(file.getName())) {
-                    listMusicName.add(file.getName());
-                }
-            }
-        }
+        if (files != null) listMusicName.addAll(Arrays.asList(files));
         return listMusicName;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-
-    //Проверяем на mp3
-    public boolean accept(String name) {
-        String mimeType = URLConnection.guessContentTypeFromName(name);
-        System.out.println(mimeType);
-        return mimeType != null && mimeType.indexOf("audio") == 0;
     }
 }
 
--- app/src/main/java/com/example/mp3message/UserPresenterImpl.java
+++ app/src/main/java/com/example/mp3message/UserPresenterImpl.java
@@ -1,12 +1,22 @@
 package com.example.mp3message;
 
+import android.os.Environment;
+
 import java.util.List;
 
 public class UserPresenterImpl implements UserPresenter {
 
+    private final String path = Environment.getExternalStorageDirectory().toString() + "/Download/";
+    //Считываем директорию, потом убираем лишние файлы(не audio)
     @Override
     public List<String> showListMusic() {
         UserModel userModel = new UserModel();
-        return userModel.readDir();
+        DeleteNotAudio deleteNotAudio = new DeleteNotAudio();
+        return deleteNotAudio.accept(userModel.readDir(path));
     }
+
+    public String getPath() {
+        return path;
+    }
+
 }
